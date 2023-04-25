a7d2aeaf857aa3141c9c6215b6064ebf6199327d
mahsa-fotros <fotros.mahsa@gmail.com>
2023-04-25 21:28:42 +0430

    persist user in app

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,12 +1,24 @@
-import { createContext , useState, useContext } from "react";
+import { createContext , useState, useContext, useEffect } from "react";
 
 
 const AuthContext = createContext();
 const AuthContextDispatcher = createContext();
 
+const LOCAL_STORAGE_AUTH_KEY= "authState";
+
 
 const AuthProvider = ({ children })=>{
     const [state, setState]= useState(false);
+    useEffect(()=>{
+        const userData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_AUTH_KEY)) || false;
+        setState(userData)
+    },[])
+
+    // useEffect(()=>{
+    //     const data = JSON.stringify(state)
+    //     localStorage.setItem(LOCAL_STORAGE_AUTH_KEY, data);
+    // },[state])
+    
 
     return(
         <AuthContext.Provider value={state}>
