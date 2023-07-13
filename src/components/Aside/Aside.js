c3b374963569afbd7c090bc7e0a31b55cb2a8212
mahsa-fotros <fotros.mahsa@gmail.com>
2023-07-13 19:26:22 +0330

    add price range mui slider to Aside.js to apply on products in SingleCategoryPage.js

--- src/components/Aside/Aside.js
+++ src/components/Aside/Aside.js
@@ -1,28 +1,54 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Slider } from "@mui/material";
 import "./aside.css"
 
-const Aside = ({selectedCategory}) => {
+const Aside = ({ selectedCategory, onPriceRangeChange }) => {
   const [categories, setCategories] = useState([]);
+  const [range, setRange] = useState([0, 1000]);
   useEffect(() => {
     axios
       .get("https://fakestoreapi.com/products/categories")
       .then((res) => setCategories(res.data));
   }, []);
 
+const handlePriceRangeChange = (event, newRange) => {
+  const rangeArray = Array.isArray(newRange) ? newRange : [newRange, newRange];
+  setRange(rangeArray);
+  onPriceRangeChange(event, rangeArray); // Call the parent function to filter products
+};
+useEffect(() => {
+  setRange([0, 1000]);
+}, [selectedCategory]);
   return (
     <aside className="asideContainer">
       <ul className="category__items sidebar__card">
-        <h4>Category</h4>
+        <h4 className="card__title">Category</h4>
         {categories.map((category) => (
           <li key={category}>
-            <Link to={`/products/category/${category.split(" ").join("-")}`} className={`categoryLinks ${category===selectedCategory && "selectedLink"}`}>
+            <Link
+              to={`/products/category/${category.split(" ").join("-")}`}
+              className={`categoryLinks ${
+                category === selectedCategory && "selectedLink"
+              }`}
+            >
               {category}
             </Link>
           </li>
         ))}
       </ul>
+      <div className="sidebar__range sidebar__card">
+        <h4 className="card__title">Price Range</h4>
+        <Slider
+          getAriaLabel={() => "price range"}
+          value={range}
+          onChange={handlePriceRangeChange}
+          valueLabelDisplay="auto"
+          min={0}
+          max={1000}
+        />
+      </div>
     </aside>
   );
 };
--- src/pages/SingleCategoryPage.js
+++ src/pages/SingleCategoryPage.js
@@ -1,77 +1,89 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom'; 
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
 import { useCart, useCartActions } from "../Context/CartProvider";
 import { toast } from "react-toastify";
 import Layout from "../Layout/Layout";
-import axios from 'axios';
+import axios from "axios";
 import { checkInCart } from "../utils/checkInCart";
 import "../App.css";
-import Aside from '../components/Aside/Aside';
+import Aside from "../components/Aside/Aside";
 
 const SingleCategoryPage = () => {
-    const { cart } = useCart();
-    const dispatch = useCartActions();
-    const [productList,setProductList] = useState([]);
-    const addProductHandler = (product) => {
-      toast.success(
-        `${product.title.split(" ").slice(0, 2).join(" ")} is added to cart !`,
-        { autoClose: 1500 }
-      );
-      dispatch({ type: "ADD_TO_CART", payload: product });
-    };
-    const params = useParams();
-    const location = useLocation();
-    const category= params.categoryName.split("-").join(" ");
+  const { cart } = useCart();
+  const dispatch = useCartActions();
+  const [productList, setProductList] = useState([]);
+  const [filteredProductList, setFilteredProductList] = useState([]);
+  const addProductHandler = (product) => {
+    toast.success(
+      `${product.title.split(" ").slice(0, 2).join(" ")} is added to cart !`,
+      { autoClose: 1500 }
+    );
+    dispatch({ type: "ADD_TO_CART", payload: product });
+  };
+  const params = useParams();
+  const location = useLocation();
+  const category = params.categoryName.split("-").join(" ");
 
-    useEffect(()=>{
-        axios.get(`https://fakestoreapi.com/products/category/${category}`).then(res=>setProductList(res.data))
-    },[location])
+  useEffect(() => {
+    axios
+      .get(`https://fakestoreapi.com/products/category/${category}`)
+      .then((res) => setProductList(res.data));
+  }, [location, category]);
 
-    return (
-      <Layout>
-        <div className='contentContainer'>
-          <Aside selectedCategory={category} />
-          <main>
-            <section className="productList">
-              {productList.map((product) => {
-                return (
-                  <section className="product" key={product.id}>
-                    <div className="productImage">
-                      <img src={product.image} alt={product.title} />
-                    </div>
-                    <div className="productDesc">
-                      <div>
-                        {product.title.split(" ").slice(0, 2).join(" ")}
-                      </div>
-                      <div className="productPrice">
-                        <p>${product.price}</p>
-                        {/* <p className="originalPrice">
+  useEffect(() => {
+    setFilteredProductList(productList);
+  }, [productList]);
+  
+  const handlePriceRangeChange = (event, newRange) => {
+    const [minPrice, maxPrice] = newRange;
+    const filteredProducts = productList.filter(
+      (product) => product.price >= minPrice && product.price <= maxPrice
+    );
+    setFilteredProductList(filteredProducts);
+  };
+
+  return (
+    <Layout>
+      <div className="contentContainer">
+        <Aside
+          selectedCategory={category}
+          onPriceRangeChange={handlePriceRangeChange}
+        />
+        <main>
+          <section className="productList">
+            {filteredProductList.map((product) => {
+              return (
+                <section className="product" key={product.id}>
+                  <div className="productImage">
+                    <img src={product.image} alt={product.title} />
+                  </div>
+                  <div className="productDesc">
+                    <div>{product.title.split(" ").slice(0, 2).join(" ")}</div>
+                    <div className="productPrice">
+                      <p>${product.price}</p>
+                      {/* <p className="originalPrice">
                       {product.discount > 0 ? <>List: ${product.price}</> : ""}
                     </p> */}
-                      </div>
                     </div>
-                    <div className="buttonContainer">
-                      <button
-                        onClick={() => addProductHandler(product)}
-                        className={`btn primary ${
-                          checkInCart(cart, product) && "third"
-                        }`}
-                      >
-                        {checkInCart(cart, product) ? "In Cart" : "Add to Cart"}
-                      </button>
-                    </div>
-                  </section>
-                );
-              })}
-            </section>
-          </main>
-        </div>
-      </Layout>
-    );
-}
- 
+                  </div>
+                  <div className="buttonContainer">
+                    <button
+                      onClick={() => addProductHandler(product)}
+                      className={`btn primary ${
+                        checkInCart(cart, product) && "third"
+                      }`}
+                    >
+                      {checkInCart(cart, product) ? "In Cart" : "Add to Cart"}
+                    </button>
+                  </div>
+                </section>
+              );
+            })}
+          </section>
+        </main>
+      </div>
+    </Layout>
+  );
+};
+
 export default SingleCategoryPage;
-
- 
-  
-
